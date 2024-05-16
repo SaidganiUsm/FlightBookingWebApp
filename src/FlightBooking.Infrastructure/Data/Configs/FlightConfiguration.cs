26faa42c580a8ecfcae0743f3521a70bda91f7ae
SaidganiUsm <saidgani110@gmail.com>
2024-05-16 14:14:50 +0500

    [1] Configuring my models

---
+++ src/FlightBooking.Infrastructure/Data/Configs/FlightConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using FlightBookingApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace FlightBookingApp.Infrastructure.Data.Configs
+{
+    public class FlightConfiguration : IEntityTypeConfiguration<Flight>
+    {
+        public void Configure(EntityTypeBuilder<Flight> builder)
+        {
+            builder.Property(r => r.DeparturePoint).HasMaxLength(50).IsRequired(true);
+            builder.Property(r => r.DestinationPoint).HasMaxLength(50).IsRequired(true);
+
+            builder.Property(r => r.StartDateTime).HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+            builder.Property(r => r.EndDateTime).HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+            builder.Property(r => r.CreationDate).HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+            builder.Property(r => r.ModificationDate).HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+        }
+    }
+}
