23464514c3b6c5cd7025f770ee97276ffaf8c3fe
SaidganiUsm <saidgani110@gmail.com>
2024-05-25 23:10:12 +0500

    [1] Pagination and base repository

---
+++ src/FlightBooking.Core/Persistence/Repositories/IQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightBookingApp.Core.Persistence.Repositories
+{
+    public interface IQuery<T>
+    {
+        IQueryable<T> Query();
+
+        IQueryable<T> QueryAsNoTracking();
+    }
+}
