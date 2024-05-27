c685f7599395715c38c330240dc323a303408b8a
SaidganiUsm <saidgani110@gmail.com>
2024-05-27 09:53:40 +0500

    [2] Email service added

---
+++ src/FlightBookingApp.Application/Common/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace FlightBookingApp.Application.Common.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string content);
+    }
+}
