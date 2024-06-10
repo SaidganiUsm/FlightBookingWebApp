efcf19682946322a297a0c56fc757fce4d195740
SaidganiUsm <saidgani110@gmail.com>
2024-06-10 12:25:53 +0500

    [10] Get single flight infomration by id query and handler

---
+++ src/FlightBookingApp.Application/Features/Flights/Admin/Query/GetbyId/GetFlightResponse.cs
@@ -0,0 +1,15 @@
+﻿using FlightBookingApp.Core.Entities;
+
+namespace FlightBookingApp.Application.Features.Flights.Admin.Query.GetbyId
+{
+    public class GetFlightResponse
+    {
+        public int Id { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public Location? DepartureLocation { get; set; }
+        public Location? DestinationLocation { get; set; }
+        public int TotalTickets { get; set; }
+        public int TicketsAvailable { get; set; }
+    }
+}
