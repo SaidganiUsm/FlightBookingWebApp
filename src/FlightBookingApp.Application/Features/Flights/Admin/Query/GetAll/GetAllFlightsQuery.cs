4fe4cf988d45de580e04be087dfff2771a24659c
SaidganiUsm <saidgani110@gmail.com>
2024-06-10 12:25:26 +0500

    [9] Get all flights query and handler

---
+++ src/FlightBookingApp.Application/Features/Flights/Admin/Query/GetAll/GetAllFlightsQuery.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using FlightBookingApp.Application.Common.DTOs;
+using FlightBookingApp.Application.Common.Interfaces.Repositories;
+using FlightBookingApp.Application.Common.Models.Requests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightBookingApp.Application.Features.Flights.Admin.Query.GetAll
+{
+    public class GetAllFlightsQuery : IRequest<GetListResponseDto<GetAllFlightsRespnse>>
+    {
+        public PageRequest PageRequest { get; set; }
+    }
+
+    public class GetAllFlightsQueryHandler : 
+        IRequestHandler<GetAllFlightsQuery, GetListResponseDto<GetAllFlightsRespnse>>
+    {
+        private readonly IFlightRepository _flightRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllFlightsQueryHandler(
+            IFlightRepository flightRepository, 
+            IMapper mapper
+        )
+        {
+            _flightRepository = flightRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetListResponseDto<GetAllFlightsRespnse>> Handle(
+            GetAllFlightsQuery request, 
+            CancellationToken cancellationToken
+        )
+        {
+            var flights = await _flightRepository.GetListAsync(
+                include: f => f.Include(f => f.Tickets)
+                .Include(f => f.DepartureLocation)
+                .Include(f => f.DestinationLocation!),
+                enableTracking: false,
+                size: request.PageRequest.PageSize,
+                index: request.PageRequest.PageIndex,
+                cancellationToken: cancellationToken
+                );
+
+            var response = _mapper.Map<GetListResponseDto<GetAllFlightsRespnse>>(flights);
+
+            return response;
+        }
+    }
+}
---
+++ src/FlightBookingApp.Application/Features/Flights/Admin/Query/GetAll/GetAllFlightsRespnse.cs
@@ -0,0 +1,20 @@
+﻿using FlightBookingApp.Core.Entities;
+
+namespace FlightBookingApp.Application.Features.Flights.Admin.Query.GetAll
+{
+    public class GetAllFlightsRespnse
+    {
+        public int Id { get; set; }
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+
+        public Location? DepartureLocation { get; set; }
+
+        public Location? DestinationLocation { get; set; }
+
+        public int TotalTickets { get; set; }
+
+        public int TicketsAvailable { get; set; }
+    }
+}
