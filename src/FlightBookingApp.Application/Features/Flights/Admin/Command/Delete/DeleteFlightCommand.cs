b7d5bdc8d9acbb2c728dacdb39afecf541d70eff
SaidganiUsm <saidgani110@gmail.com>
2024-06-11 15:12:05 +0500

    [8] Delete flight command

---
+++ src/FlightBookingApp.Application/Features/Flights/Admin/Command/Delete/DeleteFlightCommand.cs
@@ -0,0 +1,36 @@
+﻿using FlightBookingApp.Application.Common.Interfaces.Repositories;
+using FlightBookingApp.Core.Entities;
+using MediatR;
+
+namespace FlightBookingApp.Application.Features.Flights.Admin.Command.Delete
+{
+    public class DeleteFlightCommand : IRequest<DeleteFlightResponse>
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeleteFlightCommandHandler : IRequestHandler<DeleteFlightCommand, DeleteFlightResponse>
+    {
+        private readonly IFlightRepository _flightRepository;
+
+        public DeleteFlightCommandHandler(IFlightRepository flightRepository)
+        {
+            _flightRepository = flightRepository;
+        }
+
+        public async Task<DeleteFlightResponse> Handle(DeleteFlightCommand request, CancellationToken cancellationToken)
+        {
+            var flight = await _flightRepository.GetAsync(
+                f => f.Id == request.Id, 
+                cancellationToken: cancellationToken
+            );
+
+            if (flight == null)
+                return null!;
+
+            await _flightRepository.DeleteAsync(flight);
+
+            return new DeleteFlightResponse { Id = flight.Id};
+        }
+    }
+}
---
+++ src/FlightBookingApp.Application/Features/Flights/Admin/Command/Delete/DeleteFlightResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace FlightBookingApp.Application.Features.Flights.Admin.Command.Delete
+{
+    public class DeleteFlightResponse
+    {
+        public int Id { get; set; }
+    }
+}
