56ef0d13930566cf922c45316148c2de1dfeb1bc
SaidganiUsm <saidgani110@gmail.com>
2024-06-14 00:33:30 +0500

    [14] Get single ticket query

--- src/FlightBookingApp.Application/Features/Tickets/Query/GetById/GetByIdTicketQuery.cs
+++ src/FlightBookingApp.Application/Features/Tickets/Query/GetById/GetByIdTicketQuery.cs
@@ -1,5 +1,7 @@
-﻿using FlightBookingApp.Application.Common.Interfaces.Repositories;
+﻿using AutoMapper;
+using FlightBookingApp.Application.Common.Interfaces.Repositories;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightBookingApp.Application.Features.Tickets.Query.GetById
 {
@@ -12,18 +14,60 @@ namespace FlightBookingApp.Application.Features.Tickets.Query.GetById
 		: IRequestHandler<GetByIdTicketQuery, GetByIdTicketResponse>
 	{
 		private readonly ITicketRepository _ticketRepository;
+        private readonly ILocationRepository _locationRepository;
+        private readonly IFlightStatusRepository _flightStatusRepository;
+        private readonly IMapper _mapper;
 
-        public GetByIdTicketQueryHandler(ITicketRepository ticketRepository)
+        public GetByIdTicketQueryHandler(
+            ITicketRepository ticketRepository,
+            ILocationRepository locationRepository,
+            IFlightStatusRepository flightStatusRepository,
+            IMapper mapper
+        )
         {
 			_ticketRepository = ticketRepository;
+            _locationRepository = locationRepository;
+            _flightStatusRepository = flightStatusRepository;
+            _mapper = mapper;
         }
 
         public async Task<GetByIdTicketResponse> Handle(
 			GetByIdTicketQuery request, 
 			CancellationToken cancellationToken
-		)
+        )
 		{
-			throw new NotImplementedException();
-		}
+            var ticket = await _ticketRepository.GetAsync(
+                predicate: x => x.Id == request.Id,
+                include: x => x.Include(t => t.Rank)
+                    .Include(t => t.Flight)
+                    .Include(t => t.TicketStatus)!,
+                cancellationToken: cancellationToken
+            );
+
+            var departure = await _locationRepository.GetAsync(
+                    x => x.Id == ticket.Flight!.DepartureLocationId,
+                    cancellationToken: cancellationToken
+                );
+
+            ticket.Flight!.DepartureLocation = departure;
+
+            var destination = await _locationRepository.GetAsync(
+                x => x.Id == ticket.Flight.DestinationLocationId,
+                cancellationToken: cancellationToken
+            );
+
+            ticket.Flight!.DestinationLocation = destination;
+
+            var status = await _flightStatusRepository.GetAsync(
+                x => x.Id == ticket.Flight.FlightStatusId,
+                cancellationToken: cancellationToken
+            );
+
+            ticket.Flight!.FlightStatus = status;
+
+            var response = _mapper.Map<GetByIdTicketResponse>(ticket);
+
+            return response;
+        }
 	}
 }
--- src/FlightBookingApp.Application/Features/Tickets/Query/GetById/GetByIdTicketResponse.cs
+++ src/FlightBookingApp.Application/Features/Tickets/Query/GetById/GetByIdTicketResponse.cs
@@ -1,6 +1,15 @@
-﻿namespace FlightBookingApp.Application.Features.Tickets.Query.GetById
+﻿using FlightBookingApp.Application.Common.DTOs;
+
+namespace FlightBookingApp.Application.Features.Tickets.Query.GetById
 {
 	public class GetByIdTicketResponse
 	{
-	}
+        public double? Price { get; set; }
+
+        public FlightDto? Flight { get; set; }
+
+        public RankDto? Rank { get; set; }
+
+        public TicketStatusDto? TicketStatus { get; set; }
+    }
 }
